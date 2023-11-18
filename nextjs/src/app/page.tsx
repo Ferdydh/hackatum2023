63df84f7dd215a78e244379ecc0d0e04e51f2fba
ferdydh <ferdydh.hadiwijaya@tum.de>
2023-11-18 15:42:47 +0100

    fix tailwind config

--- nextjs/src/app/page.tsx
+++ nextjs/src/app/page.tsx
@@ -5,18 +5,30 @@ import { Menu } from "./_components/menu";
 
 export default function Home() {
   return (
-    <main className="min-h-screen items-center justify-center">
+    <main className="min-h-screen items-center w-screen">
       {/* <HelloComponent />
         <Input></Input> */}
 
-      <div className="w-screen h-12">
+      <div className="h-12">
         <Menu />
       </div>
-      <div className="w-screen flex flex-row">
-        <Sidebar project="testt"></Sidebar>
-        <div>
-          <div>Text Editor</div>
+      <div className="grid grid-cols-5">
+        <Sidebar project="testt" className="block col-span-2"></Sidebar>
+        <div className="col-span-3 col-start-2">
+          <div className="w-full h-full">Text Editor</div>
           <div>Console</div>
+        </div>
+      </div>
+      <div className="App">
+        <h1 className="bg-secondary">Hello CodeSandbox</h1>
+        <h2>Start editing to see some magic happen!</h2>
+        <div className="grid grid-cols-3 gap-4">
+          <div className="border col-span-1">
+            1
+          </div>
+          <div className="border col-span-1">
+            2
+          </div>
         </div>
       </div>
     </main>
