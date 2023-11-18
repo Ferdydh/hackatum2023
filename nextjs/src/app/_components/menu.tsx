e5c51ee3b9d67958dde8ad8f0c9109f61b7a5760
Furkan <furkan.imamoglu@tum.de>
2023-11-18 17:04:29 +0100

    styling

--- nextjs/src/app/_components/menu.tsx
+++ nextjs/src/app/_components/menu.tsx
@@ -14,6 +14,8 @@ import {
   MenubarTrigger,
 } from "@/components/ui/menubar";
 
+import { SlidersHorizontal, LogOut } from "lucide-react";
+
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 import { Input } from "@/components/ui/input";
@@ -25,30 +27,43 @@ import { Switch } from "@/components/ui/switch";
 
 export function Menu() {
   return (
-    <Menubar className="flex items-center justify-between">
+    <Menubar className="flex items-center justify-between ">
       <div>
-        <Label>Logo</Label>
+        <Label className="text-white">Logo</Label>
       </div>
 
-      <Input type="email" placeholder="Email" className="w-1/3" />
+      <Input
+        type="prompt"
+        placeholder="What can I do for you?"
+        className="w-1/2"
+      />
 
       <div className="flex items-center">
         <MenubarMenu>
-          <MenubarTrigger>Settings</MenubarTrigger>
+          <MenubarTrigger>
+            <SlidersHorizontal size={20} className="mr-2" />
+            Settings
+          </MenubarTrigger>
           <MenubarContent>
-            <div className="m-3 flex items-center ">
-              <Label htmlFor="dark-mode">Dark Mode</Label>
+            <div className="m-3 flex items-center">
+              <Label htmlFor="dark-mode" className="mr-5">
+                Dark Mode
+              </Label>
               <Switch id="dark-mode" />
             </div>
 
-            <MenubarSeparator />
             <MenubarItem inset>Edit...</MenubarItem>
-            <MenubarSeparator />
+
             <MenubarItem inset>Add Profile...</MenubarItem>
           </MenubarContent>
         </MenubarMenu>
 
-        <Avatar>
+        <Button variant="ghost" className=" text-white">
+          <LogOut size={20} className="mr-2" />
+          Logout
+        </Button>
+
+        <Avatar className="mx-5">
           <AvatarImage alt="@shadcn" />
           <AvatarFallback>JB</AvatarFallback>
         </Avatar>
