eca6ef64eba550dbc5ad6d94e6c74a7096daf43c
Furkan <furkan.imamoglu@tum.de>
2023-11-19 01:24:18 +0100

    Terminal Done

--- nextjs/src/app/_components/terminal.tsx
+++ nextjs/src/app/_components/terminal.tsx
@@ -1,5 +1,37 @@
+import React, { useState } from "react";
+import { Label } from "@/components/ui/label";
+import { api } from "~/trpc/react";
+import { Button } from "@/components/ui/button";
+
 export function Terminal() {
-  return <div className="h-20vh">
-    Terminal
-  </div>
+  const [command, setCommand] = useState("");
+  const [output, setOutput] = useState("");
+  const terminalExecuteMutation = api.root.terminal_execute.useMutation();
+
+  const handleExecute = async () => {
+    try {
+      const result = await terminalExecuteMutation.mutateAsync({ command });
+      setOutput(result.output);
+    } catch (error) {
+      console.error("Failed to execute command", error);
+      setOutput("Error executing command");
+    }
+  };
+
+  return (
+    <div className="h-20vh mt-8">
+      <Label className="mx-3 text-xl">Terminal</Label>
+      <div className="ml-4">
+        <pre>{output}</pre>
+        <input
+          type="text"
+          value={command}
+          onChange={(e) => setCommand(e.target.value)}
+          placeholder="_"
+          className=" w-full bg-transparent focus:outline-none"
+          onKeyDown={(e) => e.key === "Enter" && handleExecute()}
+        />
+      </div>
+    </div>
+  );
 }
--- nextjs/src/server/api/routers/mainRouter.ts
+++ nextjs/src/server/api/routers/mainRouter.ts
@@ -5,46 +5,47 @@ import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 import axios from "axios";
 import qs from "qs";
 
-const LOCAL_BACKEND_URI = "http://python:8002/v1/api/"
-const BACKEND_URI = "http://192.168.137.17:8002/v1/api/"
+const LOCAL_BACKEND_URI = "http://python:8002/v1/api/";
+const BACKEND_URI = "http://192.168.137.17:8002/v1/api/";
 
 export const mainRouter = createTRPCRouter({
-  get_project_directory: publicProcedure
-    .query(async () => {
-      const { data } = await axios.get<Directory[]>(BACKEND_URI + "get_project_directory")
+  get_project_directory: publicProcedure.query(async () => {
+    const { data } = await axios.get<Directory[]>(
+      BACKEND_URI + "get_project_directory",
+    );
 
-      // const { data } = await axios.get<Directory[]>(BACKEND_URI + "get_project_directory", {
-      //   timeout: 8000,
-      //   headers: {
-      //     Accept: "application/json", // Specify that we want JSON in return
-      //   },
-      //   paramsSerializer: (params: any) =>
-      //     qs.stringify(params, { arrayFormat: "repeat" }),
-      // });
+    // const { data } = await axios.get<Directory[]>(BACKEND_URI + "get_project_directory", {
+    //   timeout: 8000,
+    //   headers: {
+    //     Accept: "application/json", // Specify that we want JSON in return
+    //   },
+    //   paramsSerializer: (params: any) =>
+    //     qs.stringify(params, { arrayFormat: "repeat" }),
+    // });
 
-      return {
-        root: data,
-      };
-    }),
+    return {
+      root: data,
+    };
+  }),
 
   open_file: publicProcedure
     .input(z.object({ full_path: z.string() }))
     .query(async ({ input }) => {
       const { data } = await axios.post<string>(BACKEND_URI + "open_file", {
-        "full_path": input.full_path
-      })
+        full_path: input.full_path,
+      });
 
       return {
         file_content: data,
-      }
+      };
     }),
 
   new_file: publicProcedure
     .input(z.object({ full_path: z.string() }))
     .query(async ({ input }) => {
       const { data } = await axios.post<Directory[]>(BACKEND_URI + "new_file", {
-        "full_path": input.full_path
-      })
+        full_path: input.full_path,
+      });
 
       return {
         root: data,
@@ -56,9 +57,9 @@ export const mainRouter = createTRPCRouter({
     .input(z.object({ new_contents: z.string() }))
     .query(async ({ input }) => {
       const { data } = await axios.post<boolean>(BACKEND_URI + "edit_file", {
-        "full_path": input.full_path,
-        "new_contents": input.new_contents
-      })
+        full_path: input.full_path,
+        new_contents: input.new_contents,
+      });
 
       return {
         success: data,
@@ -67,15 +68,18 @@ export const mainRouter = createTRPCRouter({
 
   terminal_execute: publicProcedure
     .input(z.object({ command: z.string() }))
-    .query(async ({ input }) => {
+    .mutation(async ({ input }) => {
       type TerminalExecuteType = {
-        "output": string,
-        "new_project_directory": Directory[]
-      }
+        output: string;
+        new_project_directory: Directory[];
+      };
 
-      const { data } = await axios.post<TerminalExecuteType>(BACKEND_URI + "terminal_execute", {
-        "command": input.command,
-      })
+      const { data } = await axios.post<TerminalExecuteType>(
+        BACKEND_URI + "terminal_execute",
+        {
+          command: input.command,
+        },
+      );
 
       return {
         output: data.output,
