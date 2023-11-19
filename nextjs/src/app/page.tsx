bed56b172088894f4429bd2aa81c6dbd9f3fd0a2
Furkan <furkan.imamoglu@tum.de>
2023-11-19 05:46:50 +0100

    img add, hover fix

--- nextjs/src/app/page.tsx
+++ nextjs/src/app/page.tsx
@@ -48,31 +48,35 @@ export default function Home() {
     api.root.get_project_directory.useQuery(); // Should we make skeleton for sidebar?
 
   useEffect(() => {
-    setRootFolder(rootFolderData?.root)
-  }, [rootFolderData])
+    setRootFolder(rootFolderData?.root);
+  }, [rootFolderData]);
 
-  const [fileContent, setFileContent] = useState<string>()
-  const [fileFullPath, setFileFullPath] = useState<string>()
+  const [fileContent, setFileContent] = useState<string>();
+  const [fileFullPath, setFileFullPath] = useState<string>();
 
   const mutationOpenFile = api.root.open_file.useMutation({
     onSuccess: ({ file_content }) => {
-      setFileContent(file_content)
-    }
-  })
+      setFileContent(file_content);
+    },
+  });
 
   function handleOpenFile(fullPath: string) {
-    mutationOpenFile.mutate({ "full_path": fullPath })
-    setFileFullPath(fullPath)
+    mutationOpenFile.mutate({ full_path: fullPath });
+    setFileFullPath(fullPath);
   }
 
-  const mutationEditFile = api.root.edit_file.useMutation({ onSuccess: ({ success }) => { } })
+  const mutationEditFile = api.root.edit_file.useMutation({
+    onSuccess: ({ success }) => {},
+  });
 
   function handleSaveFile(fileContent: string) {
     // Call edit file
-    mutationEditFile.mutate({ "new_contents": fileContent, "full_path": fileFullPath! })
-    setFileContent(fileContent)
+    mutationEditFile.mutate({
+      new_contents: fileContent,
+      full_path: fileFullPath!,
+    });
+    setFileContent(fileContent);
   }
-
 
   return (
     <main className="min-h-screen w-screen items-center overflow-hidden">
@@ -83,13 +87,18 @@ export default function Home() {
         <ResizableBox
           onResize={(_, { size }) => resizeWidth(size.width)}
           handle={
-            <hr className=" absolute right-[-3px] top-[5%] z-10 h-full w-[10px] cursor-ew-resize border-solid " />
+            <hr className=" absolute right-[-3px] top-[0%] z-10 h-full w-[2px] cursor-ew-resize border-solid hover:border-r-4 hover:border-gray-400" />
           }
-          className="relative border-r-2 border-solid hover:border-r-4 hover:border-gray-400"
+          className="relative border-r-2 border-solid "
           width={300}
           axis="x"
         >
-          <Sidebar root={rootFolder} setRootFolder={setRootFolder} handleOpenFile={handleOpenFile} className="w-full"></Sidebar>
+          <Sidebar
+            root={rootFolder}
+            setRootFolder={setRootFolder}
+            handleOpenFile={handleOpenFile}
+            className="w-full"
+          ></Sidebar>
         </ResizableBox>
 
         <ResizableBox
@@ -108,7 +117,11 @@ export default function Home() {
               height={windowSize.height - 300}
               axis="y"
             >
-              <FileEditor fileFullPath={fileFullPath!} fileContent={fileContent!} handleSaveFile={handleSaveFile}></FileEditor>
+              <FileEditor
+                fileFullPath={fileFullPath!}
+                fileContent={fileContent!}
+                handleSaveFile={handleSaveFile}
+              ></FileEditor>
             </ResizableBox>
             <ResizableBox height={terminalHeight} axis="y">
               <Terminal></Terminal>
